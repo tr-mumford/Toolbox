beb73fd9f91e969d44a72afb65fc0d18e626ace3
Tyler Mumford <58514018+tr-mumford@users.noreply.github.com>
2020-04-17 11:28:39 +0200

    hc - first commit

---
+++ File_Integrity/Hash_Checker_1-0-0.ps1
@@ -0,0 +1,12 @@
+﻿$Hash1 = Get-FileHash C:\Users\user\Desktop\a.txt -Algorithm MD5 
+$Hash2 = Get-FileHash C:\Users\user\Desktop\c.txt -Algorithm MD5
+
+Write-Host "1: " $Hash1.Hash
+Write-Host "2: " $Hash2.Hash
+
+if ($Hash1.Hash -ne $Hash2.Hash) {
+    Write-Host "Files do not match"
+}
+else {
+    Write-Host "Files do match"
+}
